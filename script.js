f50bf5ecc18e5647e063ca9bd743d1f81fa8fd28
NuryanaNn <nuryan504@gmail.comescgit clear>
2022-02-05 21:37:21 +0700

    Initial Commit

--- script.js
+++ script.js
@@ -31,6 +31,48 @@ togglerMobile.onclick = () =>{
 }
 
 
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+	let reveals = document.querySelectorAll('.reveal');
+
+	for(var i = 0; i < reveals.length; i++){
+		let windowheight = window.innerHeight;
+		let revealtop = reveals[i].getBoundingClientRect().top;
+		let revealpoint = 150;
+
+		if(revealtop < windowheight - revealpoint){
+			reveals[i].classList.add('active');
+		}else{
+			reveals[i].classList.remove('active');
+		}
+	}
+}
+
+
+let list = document.querySelectorAll('.list');
+let itmBx = document.querySelectorAll('.itmBx');
+
+for( let i = 0; i<list.length; i++){
+	list[i].addEventListener('click', function(){
+		for (let j = 0; j<list.length; j++){
+			list[j].classList.remove('active');
+		}
+		this.classList.add('active');
+
+		let dataFilter = this.getAttribute('data-filter')
+
+		for (let k = 0; k < itmBx.length; k++) {
+                    itmBx[k].classList.remove('active');
+                    itmBx[k].classList.add('hide');
+
+                    if (itmBx[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+                        itmBx[k].classList.remove('hide');
+                        itmBx[k].classList.add('active');
+                    }
+                }
+	})
+}
 
 
 
